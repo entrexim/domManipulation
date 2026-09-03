1e979a38e07c5b3f2ebd177a67ee3ae88742cdb0
Aashish Kumar Singh <ashishthakur15151515@gmail.com>
2026-09-03 19:15:54 +0530

    made delete button, li,checkbox,fix previous bugs, now anyone can use it but it has problem we need to save the data somewhere so that it could not be deleted by reloading the page

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
-const taskInput = document.getElementById('taskInput');
+let taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
 
-const input = taskInput.value;
 
-addBtn.addEventListener('click', () => {
+
+function createTask() {
+
     const newtask = document.createElement('li');
-    newtask.textContent = input;
-    //remove input 
-    input = "";
-    //creating checkbox for task completion
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox';
-    checkbox.id = 'comCheckbox'
-    //creating delete button 
-    const deletbtn = document.createElement('btn');
-    deletbtn.textContent = 'delete task';
-    //appending all the elemnt that i have created
-    document.taskList.append(newtask);
-    document.taskList.append(checkbox);
-    domcument.taskList.append(deletbtn);
-
-    //working functionality of checkbox
-
-    checkbox.addEventListener('change', (event) => {
-        if (event.target.checked) {
-            //fade the respective newtask using css addclasslist
     
-        } else {
-            //unfade the respective mewtask using css removecladslist
-        }
-    })
-    //working functionality of deletbtn
-    deletbtn.addEventListener('click', () => {
-        // removing the respective newtask elements
-        newtask.remove();
-        deletbtn.remove();
-        checkbox.remove();
+    //handling input
+    let input = taskInput.value;
+    if (input == '') {
+        return;
+    } else {
+        const span = document.createElement('span')
+        span.textContent = input;
+        newtask.appendChild(span)
+        //remove input 
     
-    })
+        taskInput.value = '';
+        //creating checkbox for task completion
+        const checkbox = document.createElement('input')
+        checkbox.type = 'checkbox';
+        checkbox.id = 'Checkbox'
+        //creating delete button 
+        const deletebtn = document.createElement('button');
+        deletebtn.id = 'deletebtn'
+        deletebtn.textContent = 'delete task';
+        //appending all the elemnt that i have created
+        newtask.append(checkbox);
+        newtask.append(deletebtn);
+    
+        taskList.appendChild(newtask);
+    
+        //working functionality of checkbox
+    
+        checkbox.addEventListener('change', (event) => {
+            if (event.target.checked) {
+                //fade the respective newtask using css addclasslist
+                span.classList.add('completed')
+    
+            } else {
+                //unfade the respective mewtask using css removecladslist
+                span.classList.remove('completed')
+            }
+        })
+        //working functionality of deletbtn
+        deletebtn.addEventListener('click', () => {
+            // removing the respective newtask elements
+            newtask.remove();
+            deletebtn.remove();
+            checkbox.remove();
+    
+        })
+        
+    }
 
+
+
+
+}
+
+addBtn.addEventListener('click',createTask);
+taskInput.addEventListener('keydown',(event)=>{
+    if(event.key ==='Enter'){
+        createTask();
+    }
 })
+
 
 
